5b7e22f31efccd96cf5a0f6f8c9ee1eda473151e
umut-duman <umutdmniv@gmail.com>
2022-12-26 02:11:59 +0300

    modified:   scripts/userinterface/ssh_configuration_ui.sh

--- scripts/userinterface/ssh_configuration_ui.sh
+++ scripts/userinterface/ssh_configuration_ui.sh
@@ -8,7 +8,7 @@ DEST="/etc/ssh/sshd_config"
 apt-get update
 apt-get install openssh-server
 
-# Disable password authentication
+# Ask and accordingly disable password authentication
 sed -i "s/#PasswordAuthentication no/PasswordAuthentication no/g" "$DEST"
 
 if (whiptail --title "$TITLE" --yesno "Do you want to disable password authentication?" 16 64); then
@@ -21,10 +21,10 @@ else
   whiptail --title "$TITLE" --msgbox "Password authentication enabled" 16 64
 fi
 
-# Disable root login
+# Ask and accordingly disable root login
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" "$DEST"
 
-if(whiptail --title "$TITLE" --yesno "Do you want to disable root login?" 16 64); then
+if (whiptail --title "$TITLE" --yesno "Do you want to disable root login?" 16 64); then
   sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" "$DEST"
 
   whiptail --title "$TITLE" --msgbox "Root login disabled" 16 64
@@ -35,6 +35,18 @@ else
 fi
 
 
+# Check and see if .ssh exists and ask to create if it is not
+if [ ! -d "$HOME/.ssh" ]; then
+  if (whiptail --title "$TITLE" --yesno "$HOME/.ssh does not exist. Do you want to create it?" 16 64); then
+    mkdir $HOME/.ssh
+    touch $HOME/.ssh/id_rsa
+    touch $HOME/.ssh/id_rsa.pub
+
+    whiptail --title "$TITLE" --msgbox "Created the directory $HOME/.ssh" 16 64
+  else
+    whiptail --title "$TITLE" --msgbox "Did not create directory $HOME/.ssh" 16 64
+  fi
+fi
 
 # Restart sshd
 systemctl restart sshd.service
